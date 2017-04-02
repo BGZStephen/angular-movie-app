3b01e5273cfc4119cae145d6dfb10f179a29f5ba
Stephen Wright <sjw948@gmail.com>
2017-04-02 20:44:39 +0100

    setup project scaffolding
    components: movies, services: movie-search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
